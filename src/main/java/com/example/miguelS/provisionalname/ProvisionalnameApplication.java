c894e2fc83965e30a7107b2669a626892dfcb684
miguel sambade martinez <miguelkilmas@gmail.com>
2023-06-11 18:10:04 +0200

    Initial commit

---
+++ src/main/java/com/example/miguelS/provisionalname/ProvisionalnameApplication.java
@@ -0,0 +1,13 @@
+package com.example.miguelS.provisionalname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisionalnameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvisionalnameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miguelS/provisionalname/ProvisionalnameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miguelS.provisionalname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvisionalnameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
